3024810a9c95eb608f794bf5ac445d05ad3843f9
maoyizeng <maoyizeng@gmail.com>
2014-06-30 09:54:46 +0800

    Model for equipment

---
+++ Volkswagen/Models/EquipmentModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Volkswagen.Models
+{
+    // 用作 EquipmentController 操作的参数的模型。
+
+    public class EquipmentModelsDefault1
+    {
+        [Display(Name = "设备编号")]
+        public string EquipmentNumber { get; set; }
+        
+        //TODO more
+    }
+}
