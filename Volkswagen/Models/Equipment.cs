4fcbcfe3c43c1c2631db12917c2b9e6dbb7b7afd
Langxiaohui <arthas_king@hotmail.com>
2014-07-04 14:33:48 +0800

    model1.edmx路径变更

--- Volkswagen/Models/Equipment.cs
+++ Volkswagen/Models/Equipment.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Volkswagen
+namespace Volkswagen.Models
 {
     using System;
     using System.Collections.Generic;
