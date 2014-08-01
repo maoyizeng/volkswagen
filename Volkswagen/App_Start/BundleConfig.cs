7da5515681b985bc98240e688fc32e348ecaaf7c
maoyizeng <maoyizeng@gmail.com>
2014-08-01 23:37:21 +0800

    file upload format

--- Volkswagen/App_Start/BundleConfig.cs
+++ Volkswagen/App_Start/BundleConfig.cs
@@ -37,11 +37,13 @@ namespace Volkswagen
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/respond.js",
+                "~/Scripts/fileinput.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/bootstrap.css",
-                 "~/Content/Site.css"));
+                 "~/Content/Site.css",
+                 "~/Content/fileinput.css"));
         }
     }
 }
